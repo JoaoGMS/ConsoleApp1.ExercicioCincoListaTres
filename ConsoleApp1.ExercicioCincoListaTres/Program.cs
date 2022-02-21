3eda2575fc9b47cac30d22adea1e3a0645059fc8
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-21 14:12:35 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioCincoListaTres/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioCincoListaTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.Write("Digite o valor inicial: ");
+            string strValor = Console.ReadLine();
+            int valor = Convert.ToInt32(strValor);
+
+            int fatorial = 1;
+
+            Console.Write(valor + "! = ");
+
+            for (int i = valor; i > 0; i--)
+            {
+                if (i != 1)
+                    Console.Write(i + " x ");
+                else
+                    Console.Write(i);
+
+                fatorial = fatorial * i;
+            }
+
+            Console.Write(" = " + fatorial);
+
+            Console.ReadKey();
+        }
+    }
+}
